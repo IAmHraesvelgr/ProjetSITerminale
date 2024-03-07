22b4a7ddd72a537b5c192a29c7bd0be57b0584e3
IAmHraesvelgr <alexis.kahne@free.fr>
2024-03-07 14:37:05 +0100

    project improvements

--- mon_projet/mon_projet.ino
+++ mon_projet/mon_projet.ino
@@ -1,12 +1,31 @@
-int switchState = 0;
+#include <LiquidCrystal.h>
+
+bool switchLampState = false;
+bool circuitOn = true;
+
+char* onText = "ON";
+char* offText = "OFF";
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 void setup() {
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
+
+  lcd.begin(16, 2);
+
+  if (circuitOn) {
+    lcd.setCursor(0, 0);
+    lcd.print(onText);
+  }
+  else {
+    lcd.setCursor(0, 0);
+    lcd.print(offText);
+  }
 }
 
 void loop() {
-  if (switchState == 0) {
+  if (!switchLampState) {
     digitalWrite(0, HIGH);
   }
 }
