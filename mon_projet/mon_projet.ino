10c695c641d1ef67df3ba9de38f89f4e60cb18f1
Hræsvelgr <alexis.kahne@free.fr>
2024-03-11 08:45:31 +0100

    format

--- mon_projet/mon_projet.ino
+++ mon_projet/mon_projet.ino
@@ -8,24 +8,30 @@ char* offText = "OFF";
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
-void setup() {
+void setup() 
+{
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
 
   lcd.begin(16, 2);
 
-  if (circuitOn) {
+  if (circuitOn) 
+  {
     lcd.setCursor(0, 0);
     lcd.print(onText);
   }
-  else {
+  
+  else 
+  {
     lcd.setCursor(0, 0);
     lcd.print(offText);
   }
 }
 
-void loop() {
-  if (!switchLampState) {
+void loop() 
+{
+  if (!switchLampState) 
+  {
     digitalWrite(0, HIGH);
   }
 }
