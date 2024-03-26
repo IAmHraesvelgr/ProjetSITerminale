41507b9951e8cf542819131382e939a8b4556933
Hræsvelgr <alexis.kahne@free.fr>
2024-03-26 09:28:07 +0100

    fin

--- mon_projet/mon_projet.ino
+++ mon_projet/mon_projet.ino
@@ -1,8 +1,23 @@
 #include <Adafruit_LiquidCrystal.h>
 
-char* onText = "ON";
+#define ONTEXT "ON "
+#define OFFTEXT "OFF"
 
 Adafruit_LiquidCrystal lcd(0);
+
+void printOn()
+{
+  digitalWrite(3, HIGH);
+  lcd.setCursor(0, 0);
+  lcd.print(ONTEXT);
+}
+
+void printOff()
+{
+  digitalWrite(3, LOW);
+  lcd.setCursor(0, 0);
+  lcd.print(OFFTEXT);
+}
 
 void setup()
 {
@@ -10,17 +25,12 @@ void setup()
   pinMode(2, INPUT);
   
   lcd.begin(16, 2);
-  lcd.print(onText);
 }
 
 void loop() 
 {
   if (digitalRead(2) == HIGH)
-  {
-    digitalWrite(3, HIGH);
-  }
+	  printOn();
   else
-  {
-    digitalWrite(3, LOW);
-  }
+	  printOff();
 }
